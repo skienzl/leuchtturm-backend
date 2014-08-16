e644bbf10423f0fd4dd3494e798a70760e9789fc
Martin Gratzer <mgratzer@gmail.com>
2014-08-16 08:17:56 +0200

    Removed value from activity properties configuration.

--- db/migrate/20140711125613_create_properties.rb
+++ db/migrate/20140711125613_create_properties.rb
@@ -2,7 +2,6 @@ class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
       t.string :key, null: false
-      t.string :value, null: false
       t.integer :activity_id, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20140815112048) do
 
   create_table "properties", force: true do |t|
     t.string   "key",         null: false
-    t.string   "value",       null: false
     t.integer  "activity_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
