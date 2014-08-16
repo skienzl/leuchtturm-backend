3e5eec4477ec2743359bce8e55a55068d8221f80
Martin Gratzer <mgratzer@gmail.com>
2014-08-16 08:46:36 +0200

    Added empty "home" controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  root to: "home#index"
+
   namespace :api, defaults: {format: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
 
