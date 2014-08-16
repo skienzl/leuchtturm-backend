353be98a1f009e899cb109977ee3659cc57ee890
Martin Gratzer <mgratzer@gmail.com>
2014-08-16 18:37:12 +0200

    Restricting oauth provider access for non admins.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -9,12 +9,18 @@ Doorkeeper.configure do
     current_user || warden.authenticate!(:scope => :user)
   end
 
+  resource_owner_from_credentials do |routes|
+    request.params[:user] = {:email => request.params[:username], :password => request.params[:password]}
+    request.env["devise.allow_params_authentication"] = true
+    request.env["warden"].authenticate!(:scope => :user)
+  end
+
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
+  admin_authenticator do
   #   # Put your admin authentication logic here.
   #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+    current_user.isAdmin || redirect_to(new_session_path(current_user))
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
