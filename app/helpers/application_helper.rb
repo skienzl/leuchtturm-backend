dc48beb19618e7b5718b0ebc5b07ead9d4c38750
Martin Gratzer <mgratzer@gmail.com>
2014-08-17 13:06:10 +0200

    Tiny "active" link fix.
    Still not working for subroutes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options)
     link_to(name, options, html_options, &block)
   end
+
+  def active_li_link_to(name = nil, options = {}, html_options = {}, &block)
+    active_class = html_options[:active] || "active"
+    html_options.delete(:active)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options)
+    content_tag(:li, link_to(name, options, html_options, &block), html_options)
+  end
 end
