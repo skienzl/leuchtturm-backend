8c8025579db78e53c37aae8940be2e56e196f7a8
Martin Gratzer <mgratzer@gmail.com>
2014-08-24 11:49:21 +0200

    Harbor model validations.

--- app/models/harbor.rb
+++ app/models/harbor.rb
@@ -1,4 +1,9 @@
 class Harbor < ActiveRecord::Base
   has_many :collections
   has_many :tasks
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 5, maximum: 20 }
+
 end
--- test/models/harbor_test.rb
+++ test/models/harbor_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class HarborTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "correct name" do
+    harbor = Harbor.new(name: "Harbor 1")
+    assert harbor.valid?
+  end
+
+  test "name required" do
+    harbor = Harbor.new()
+    assert !harbor.valid?
+    assert harbor.errors[:name].any?
+  end
+
+  test "name needs to be unique" do
+    harbor = Harbor.new(name: "Harbor 1")
+    assert harbor.valid?
+    harbor.save
+    harbor = Harbor.new(name: "Harbor 1")
+    assert !harbor.valid?
+    assert harbor.errors[:name].any?
+  end
+
+  test "name length needs to be greater than 5" do
+    harbor = Harbor.new(name: "yxz")
+    assert !harbor.valid?
+    assert harbor.errors[:name].any?
+  end
+
+  test "name length needs to be smaller than 20" do
+    harbor = Harbor.new(name: "1234567891011121314151617181920")
+    assert !harbor.valid?
+    assert harbor.errors[:name].any?
+  end
+
 end
