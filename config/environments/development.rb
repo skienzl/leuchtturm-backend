470c63d32891d10333234f3a5e4b825d2e75d086
skienzl <kienzlstefan@gmail.com>
2014-08-25 10:47:41 +0200

    Access-Control-Allow-Origin
    Add Access-Control-Allow-Origin for local testing
    
    Signed-off-by: skienzl <kienzlstefan@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,9 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
 end
