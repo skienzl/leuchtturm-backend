037596d2cd0be9715d2ccaab6ae74721160fc612
skienzl <kienzlstefan@gmail.com>
2014-08-25 16:57:51 +0200

    lower nesting level to 1

--- config/routes.rb
+++ config/routes.rb
@@ -11,19 +11,14 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
 
-      resources :harbors do
-        resources :collections do
-          resources :regions
-        end
-        resources :tasks do
-          resources :activities do
-            resources :properties
-            resources :ports
-          end
-          resources :mappings
-        end
-      end
-    end
+      resources :harbors 
+      resources :collections 
+      resources :regions
+      resources :tasks 
+      resources :activities 
+      resources :properties
+      resources :ports
+      resources :mappings
   end
-
+end
 end
