9a0386f7f8c4cb0ed05e959c24162d5ccc86f4f5
Martin Gratzer <mgratzer@gmail.com>
2014-08-29 07:59:05 +0200

    Collection model validations.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,11 @@ class Collection < ActiveRecord::Base
   belongs_to :harbor
   has_many :regions
 
+  validates :harbor, presence: true
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 5, maximum: 20 }
+
   def check_list_consistency
     true
   end
--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -2,6 +2,30 @@ require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
 
+  test "correct name" do
+    zell = harbors(:zell)
+    collection = Collection.new(harbor: zell, name: "Collection 1")
+    assert collection.valid?
+  end
+
+  test "name required" do
+    collection = Collection.new()
+    assert !collection.valid?
+    assert collection.errors[:name].any?
+  end
+
+  test "name length needs to be greater than 5" do
+    collection = Collection.new(name: "yxz")
+    assert !collection.valid?
+    assert collection.errors[:name].any?
+  end
+
+  test "name length needs to be smaller than 20" do
+    collection = Collection.new(name: "1234567891011121314151617181920")
+    assert !collection.valid?
+    assert collection.errors[:name].any?
+  end
+
   test "should have harbor" do
     zell = harbors(:zell)
     schmitten = collections(:schmitten)
