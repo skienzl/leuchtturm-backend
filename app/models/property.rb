69e5306b22d19038634570efd43b2372fdad9c4a
Martin Gratzer <mgratzer@gmail.com>
2014-08-29 16:59:41 +0200

    Property model validations.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,9 @@
 class Property < ActiveRecord::Base
   belongs_to :activity
+
+  validates :activity, presence: true
+  validates :key, presence: true
+  validates :key, uniqueness: true
+  validates :key, length: { :in => 5..20 }
+
 end
--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class PropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "activity required" do
+    property = Property.new()
+    assert !property.valid?
+    assert property.errors[:activity].any?
+  end
+
+  test "correct key" do
+    activity = activities(:one)
+    property = Property.new(key: "Property 1", activity: activity)
+    assert property.valid?
+  end
+
+  test "key required" do
+    property = Property.new()
+    assert !property.valid?
+    assert property.errors[:key].any?
+  end
+
+  test "key needs to be unique" do
+    activity = activities(:one)
+    property = Property.new(key: "Property 1", activity: activity)
+    assert property.valid?
+    property.save
+    property = Property.new(key: "Property 1", activity: activity)
+    assert !property.valid?
+    assert property.errors[:key].any?
+  end
+
+  test "key length needs to be greater than 5" do
+    activity = activities(:one)
+    property = Property.new(key: "yxz", activity: activity)
+    assert !property.valid?
+    assert property.errors[:key].any?
+  end
+
+  test "key length needs to be smaller than 20" do
+    activity = activities(:one)
+    property = Property.new(key: "1234567891011121314151617181920", activity: activity)
+    assert !property.valid?
+    assert property.errors[:key].any?
+  end
 end
