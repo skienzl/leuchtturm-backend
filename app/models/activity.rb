078ea0a3a2f621f3a42c72dbf306eafa8748006f
Martin Gratzer <mgratzer@gmail.com>
2014-08-29 11:38:16 +0200

    Activities model validations.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,10 @@ class Activity < ActiveRecord::Base
   belongs_to :task
   has_many :ports
   has_many :properties
+
+  validates :task, presence: true
+  validates :identifier, presence: true
+  validates :name, presence: true
+  validates :name, length: { :in => 5..20 }
+
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "correct name" do
+    task = tasks(:task_beacontask1)
+    activity = Activity.new(name: "Activity 1", task: task, identifier: "abcdefg")
+    assert activity.valid?
+  end
+
+  test "name required" do
+    activity = Activity.new()
+    assert !activity.valid?
+    assert activity.errors[:name].any?
+  end
+
+  test "task required" do
+    activity = Activity.new()
+    assert !activity.valid?
+    assert activity.errors[:task].any?
+  end
+
+  test "identifier required" do
+    activity = Activity.new()
+    assert !activity.valid?
+    assert activity.errors[:identifier].any?
+  end
+
+  test "name length needs to be greater than 5" do
+    task = tasks(:task_beacontask1)
+    activity = Activity.new(name: "yxz", task: task, identifier: "abcdefg")
+    assert !activity.valid?
+    assert activity.errors[:name].any?
+  end
+
+  test "name length needs to be smaller than 20" do
+    task = tasks(:task_beacontask1)
+    activity = Activity.new(name: "1234567891011121314151617181920", task: task, identifier: "abcdefg")
+    assert !activity.valid?
+    assert activity.errors[:name].any?
+  end
 end
