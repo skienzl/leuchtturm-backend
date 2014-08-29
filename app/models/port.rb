e3295442e605e1c112696759fab4cb18f9921079
Martin Gratzer <mgratzer@gmail.com>
2014-08-29 16:53:49 +0200

    Port model validations.

--- app/models/port.rb
+++ app/models/port.rb
@@ -13,4 +13,16 @@ class Port < ActiveRecord::Base
   belongs_to :activity
   belongs_to :in_mapping, class_name: "Mapping", foreign_key: "out_id"
   belongs_to :out_mapping, class_name: "Mapping", foreign_key: "in_id"
+
+  validates :data_type, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 1,
+  }
+
+  validates :port_type, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 2,
+  }
 end
--- test/models/port_test.rb
+++ test/models/port_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class PortTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "datatype can be string" do
+    port = ports(:one)
+    port.data_type = DataType::STRING
+    assert port.valid?
+  end
+
+  test "datatype can be integer" do
+    port = ports(:one)
+    port.data_type = DataType::INTEGER
+    assert port.valid?
+  end
+
+  test "datatype out of scope" do
+    port = ports(:one)
+    port.data_type = -1
+    assert !port.valid?
+    assert port.errors[:data_type].any?
+
+    port.data_type = 2
+    assert !port.valid?
+    assert port.errors[:data_type].any?
+  end
+
+  test "port type can be in" do
+    port = ports(:one)
+    port.port_type = PortType::IN
+    assert port.valid?
+  end
+
+  test "port type can be out" do
+    port = ports(:one)
+    port.port_type = PortType::OUT
+    assert port.valid?
+  end
+
+  test "port type can be proxy" do
+    port = ports(:one)
+    port.port_type = PortType::PROXY
+    assert port.valid?
+  end
+
+  test "port type out of scope" do
+    port = ports(:one)
+    port.port_type = -1
+    assert !port.valid?
+    assert port.errors[:port_type].any?
+
+    port.port_type = 3
+    assert !port.valid?
+    assert port.errors[:port_type].any?
+  end
+
 end
