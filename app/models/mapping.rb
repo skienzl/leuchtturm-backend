abe720892f0d21dc9323fc90041ff574ddf3dbd0
Martin Gratzer <mgratzer@gmail.com>
2014-08-29 16:18:29 +0200

    Mapping model validation.

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -1,17 +1,34 @@
 class Mapping < ActiveRecord::Base
-  has_one :in, class_name: "Port", foreign_key: "in_id"
-  has_one :out, class_name: "Port", foreign_key: "out_id"
+  has_one :in, class_name: 'Port', foreign_key: 'id'
+  has_one :out, class_name: 'Port', foreign_key: 'id'
   belongs_to :task
 
-  validate :validate_port_types, :validate_port_activities
+  validates :task, presence: true
+  validates :in, presence: true, if: 'self.out.nil?'
+  validates :out, presence: true, if: 'self.in.nil?'
+
+  validate :validate_port_types, :validate_port_data_types
 
   private
-    def validate_port_types
-      true
+
+    # VALIDATION HELPERS
+
+    def validate_port_data_types
+      if !self.in.nil? && !self.out.nil?
+        if self.in.data_type != self.out.data_type
+          errors.add(:base, 'In and out port data types must match')
+        end
+      end
     end
 
-    def validate_port_activities
-      true
+    def validate_port_types
+      if !self.in.nil? && self.in.port_type != PortType::IN
+        errors.add(:in, 'has an incorrect port type')
+      end
+
+      if !self.out.nil? && self.out.port_type != PortType::OUT
+        errors.add(:out, 'has an incorrect port type')
+      end
     end
 
 end
--- test/models/mapping_test.rb
+++ test/models/mapping_test.rb
@@ -1,7 +1,87 @@
 require 'test_helper'
 
 class MappingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "task required" do
+    mapping = Mapping.new()
+    assert !mapping.valid?
+    assert mapping.errors[:task].any?
+  end
+
+  test "in required if out missing" do
+    mapping = Mapping.new()
+    assert !mapping.valid?
+    assert mapping.errors[:out].any?
+    assert mapping.errors[:in].any?
+  end
+
+  test "out can be nil if in is set" do
+    task = tasks(:task_beacontask1)
+    port = ports(:one)
+    port.port_type = PortType::IN
+    mapping = Mapping.new(task: task, in: port)
+    assert mapping.valid?, mapping.errors.full_messages
+  end
+
+  test "out required if in missing" do
+    mapping = Mapping.new()
+    assert !mapping.valid?
+    assert mapping.errors[:in].any?
+    assert mapping.errors[:out].any?
+  end
+
+  test "in can be nil if out is set" do
+    task = tasks(:task_beacontask1)
+    port = ports(:one)
+    port.port_type = PortType::OUT
+    mapping = Mapping.new(task: task, out: port)
+    assert mapping.valid?
+  end
+
+  test "in and out data types must match" do
+    task = tasks(:task_beacontask1)
+    in_port = ports(:one)
+    in_port.port_type = PortType::IN
+    in_port.data_type = DataType::STRING
+    out_port = ports(:two)
+    in_port.port_type = PortType::OUT
+    out_port.data_type = DataType::INTEGER
+
+    mapping = Mapping.new(task: task, in: in_port, out: out_port)
+
+    assert !mapping.valid?, mapping.errors.full_messages
+    assert mapping.errors.full_messages.include?('In and out port data types must match')
+  end
+
+  test "in and out data types match" do
+    task = tasks(:task_beacontask1)
+    in_port = ports(:one)
+    in_port.port_type = PortType::IN
+    in_port.data_type = DataType::STRING
+    out_port = ports(:two)
+    out_port.port_type = PortType::OUT
+    out_port.data_type = DataType::STRING
+
+    mapping = Mapping.new(task: task, in: in_port, out: out_port)
+
+    assert mapping.valid?, mapping.errors.full_messages
+  end
+
+  test "in port type must match" do
+    task = tasks(:task_beacontask1)
+    port = ports(:one)
+    port.port_type = PortType::OUT
+    mapping = Mapping.new(task: task, in: port)
+    assert !mapping.valid?, mapping.errors.full_messages
+    assert mapping.errors.full_messages.include?('In has an incorrect port type')
+  end
+
+  test "out port type must match" do
+    task = tasks(:task_beacontask1)
+    port = ports(:one)
+    port.port_type = PortType::IN
+    mapping = Mapping.new(task: task, out: port)
+    assert !mapping.valid?, mapping.errors.full_messages
+    assert mapping.errors.full_messages.include?('Out has an incorrect port type')
+  end
+
 end
