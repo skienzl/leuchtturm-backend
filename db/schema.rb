5e9dc7f931e883f8fcdb3772b8af95a82f08d48c
Martin Gratzer <mgratzer@gmail.com>
2014-09-05 16:59:21 +0200

    Settings API-Controller.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140816150410) do
+ActiveRecord::Schema.define(version: 20140905144031) do
 
   create_table "activities", force: true do |t|
     t.string   "identifier",               null: false
