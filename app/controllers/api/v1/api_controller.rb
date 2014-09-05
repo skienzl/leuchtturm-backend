d1421ac400081fae8455ceabfc6526eb2198bedf
Martin Gratzer <mgratzer@gmail.com>
2014-09-05 07:35:44 +0200

    Object not found and model validation error responses.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,13 +1,25 @@
+module ApiError
+  NOT_FOUND         = 1
+  VALIDATION_FAILED = 2
+end
+
 class Api::V1::ApiController < ActionController::Base
   #doorkeeper_for :all
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/vnd.myapp.v1' }
   respond_to :json
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  API_ERROR_CODES = {
+      ApiError::NOT_FOUND         => 'Object not found',
+      ApiError::VALIDATION_FAILED => 'Object validation failed'
+  }
+
   def create_save_with_response(object)
     if object.save
       render json: object, status: :created
     else
-      render json: object.errors, status: :unprocessable_entity
+      render json: {error: object.errors, code: ApiError::VALIDATION_FAILED}, status: :unprocessable_entity
     end
   end
 
@@ -15,8 +27,13 @@ class Api::V1::ApiController < ActionController::Base
     if object.update(parameters)
       render json: object, status: :ok
     else
-      render json: object.errors, status: :unprocessable_entity
+      render json: {error: object.errors, code: ApiError::VALIDATION_FAILED}, status: :unprocessable_entity
     end
   end
+
+  private
+    def record_not_found
+      render json: {error: 'Object not found.', code: ApiError::NOT_FOUND}, status: :not_found
+    end
 
 end
